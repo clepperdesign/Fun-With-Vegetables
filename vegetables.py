a73b5ca8b41e7280f97039cbc8f551450131903a
Clepper Design <33584689+clepperdesign@users.noreply.github.com>
2019-07-24 18:43:48 -0700

    Now gives an entire range for the harvest

--- vegetables.py
+++ vegetables.py
@@ -1,13 +1,16 @@
 import math
 lastfrost = {3 : 150, 4 : 150, 5 : 119, 6 : 119, 7 : 119, 8 : 88, 9 : 58, 10 : 30}
 #days into the year of a zone's maximum last average frost
-veg = {'radish':20,'bean':45,'melon':65,'potato':80}
-#first available harvest date from time of planting of given produce type from data set
+vegearly = {'radish':20,'bean':45,'melon':65,'potato':80}
+veglate = {'radish':70,'bean':100,'melon':110,'potato':120}
+#first and last available harvest date from time of planting of given produce type from data set
 def main():
     yourzone = int(input('what usda hardiness zone do you live in? \n'))
     vegchoice = str.lower((input('Choose a vegetable: bean, melon, radish, potato \n')))
-    test = (math.ceil((lastfrost[yourzone]+veg[vegchoice])/30))
-    usdastart = (lastfrost[yourzone]/30)
+    harveststart = (math.ceil((lastfrost[yourzone]+vegearly[vegchoice])/30))
+    harveststop = (math.ceil((lastfrost[yourzone]+veglate[vegchoice])/30))
+    usdastart = (math.ceil(lastfrost[yourzone]/30))
+    print(usdastart)
     def monthmath(x):
         month = "Month"
         if x == 1:
@@ -37,7 +40,8 @@ def main():
         else:
             month = "An error has occurred"
         return(month)
-    harvestmonth = monthmath(test)
+    harvest0 = monthmath(harveststart)
+    harvest1 = monthmath(harveststop)
     plantmonth = monthmath(usdastart)
-    print("You may plant in " + plantmonth + ". You can begin to harvest your " + vegchoice + " in " + harvestmonth + ".")
+    print("You may start planting in " + plantmonth + ". You can harvest a " + vegchoice + " crop between " + harvest0 + " and " + harvest1 + ".")
 main()
