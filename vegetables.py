acfe215767f0e16f94584052d1e9c90143484416
Clepper Design <33584689+clepperdesign@users.noreply.github.com>
2019-08-12 21:44:18 -0700

    Condensed 'month' code

--- vegetables.py
+++ vegetables.py
@@ -3,6 +3,7 @@ lastfrost = {3 : 150, 4 : 150, 5 : 119, 6 : 119, 7 : 119, 8 : 88, 9 : 58, 10 : 3
 #days into the year of a zone's maximum last average frost
 vegearly = {'radish':20,'bean':45,'melon':65,'potato':80}
 veglate = {'radish':70,'bean':100,'melon':110,'potato':120}
+monthlist = [None, "January","February","March","April","May","June","July","August","September","October","November","December"]
 #first and last available harvest date from time of planting of given produce type from data set
 def main():
     yourzone = int(input('what usda hardiness zone do you live in? \n'))
@@ -10,35 +11,8 @@ def main():
     harveststart = (math.ceil((lastfrost[yourzone]+vegearly[vegchoice])/30))
     harveststop = (math.ceil((lastfrost[yourzone]+veglate[vegchoice])/30))
     usdastart = (math.ceil(lastfrost[yourzone]/30))
-    print(usdastart)
     def monthmath(x):
-        month = "Month"
-        if x == 1:
-            month = "January"
-        elif x == 2:
-            month = "February"
-        elif x == 3:
-            month = "March"
-        elif x == 4:
-            month = "April"
-        elif x == 5:
-            month = "May"
-        elif x == 6:
-            month = "June"
-        elif x == 7:
-            month = "July"
-        elif x == 8:
-            month = "August"
-        elif x == 9:
-            month = "September"
-        elif x == 10:
-            month = "October"
-        elif x == 11:
-            month = "November"
-        elif x == 12:
-            month = "December"
-        else:
-            month = "An error has occurred"
+        month= monthlist[x]
         return(month)
     harvest0 = monthmath(harveststart)
     harvest1 = monthmath(harveststop)
