240602c2b3d91800ce3a860dd2256cb3eee34e32
Clepper Design <33584689+clepperdesign@users.noreply.github.com>
2019-08-12 21:49:21 -0700

    Condensed vegetable code

--- vegetables.py
+++ vegetables.py
@@ -1,15 +1,13 @@
 import math
 lastfrost = {3 : 150, 4 : 150, 5 : 119, 6 : 119, 7 : 119, 8 : 88, 9 : 58, 10 : 30}
 #days into the year of a zone's maximum last average frost
-vegearly = {'radish':20,'bean':45,'melon':65,'potato':80}
-veglate = {'radish':70,'bean':100,'melon':110,'potato':120}
+veglist = {'radish':[20,70],'bean':[45,100],'melon':[65,110],'potato':[80,120]}
 monthlist = [None, "January","February","March","April","May","June","July","August","September","October","November","December"]
-#first and last available harvest date from time of planting of given produce type from data set
 def main():
     yourzone = int(input('what usda hardiness zone do you live in? \n'))
     vegchoice = str.lower((input('Choose a vegetable: bean, melon, radish, potato \n')))
-    harveststart = (math.ceil((lastfrost[yourzone]+vegearly[vegchoice])/30))
-    harveststop = (math.ceil((lastfrost[yourzone]+veglate[vegchoice])/30))
+    harveststart= (math.ceil((lastfrost[yourzone]+veglist[vegchoice][0])/30))
+    harveststop = (math.ceil((lastfrost[yourzone]+veglist[vegchoice][1])/30))
     usdastart = (math.ceil(lastfrost[yourzone]/30))
     def monthmath(x):
         month= monthlist[x]
